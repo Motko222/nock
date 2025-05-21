367fdfcdd20e2698b49665cd850b662e32915bf2
Motko <77466238+Motko222@users.noreply.github.com>
2025-05-21 15:02:04 +0200

    Delete start-leader.sh

--- start-leader.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-path=$(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd) 
-folder=$(echo $path | awk -F/ '{print $NF}')
-
-systemctl restart nock-leader.service
-
-journalctl -n 200 -u nock-leader.service -f --no-hostname -o cat
