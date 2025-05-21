e3c24dfe7eb2dbbbcf05c9fb02ca6b102ce04592
Motko <77466238+Motko222@users.noreply.github.com>
2025-05-21 15:02:45 +0200

    Delete log-leader.sh

--- log-leader.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-path=$(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd) 
-folder=$(echo $path | awk -F/ '{print $NF}')
-
-journalctl -n 200 -u nock-leader.service -f --no-hostname -o cat
