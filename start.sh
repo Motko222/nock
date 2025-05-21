05c19229dc9f241029fb8f407dce6068b498e12a
Motko <77466238+Motko222@users.noreply.github.com>
2025-05-21 11:27:14 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,8 +4,8 @@ path=$(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd)
 folder=$(echo $path | awk -F/ '{print $NF}')
 
 systemctl stop nock-leader.service
-systemctl stop nock-follower.service
+#systemctl stop nock-follower.service
 
 systemctl restart nock-leader.service
-systemctl restart nock-follower.service
+#systemctl restart nock-follower.service
 
