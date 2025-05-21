408ca3724d253c58c50826341e04b909860ae560
Motko <77466238+Motko222@users.noreply.github.com>
2025-05-21 21:35:06 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ EnvironmentFile=/root/scripts/nock/env
 Environment="PATH=/root/.cargo/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin:/root/go/bin:/usr/local/go/bin:/root/go/bin:/usr/local/go/bin:/root/go/bin"
 User=root
 Group=root
-ExecStart=make run-nockchain-follower
+ExecStart=make run-nockchain
 Restart=always
 RestartSec=30
 LimitNOFILE=65536
