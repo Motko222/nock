dbc933cbb3a867ef8e449d08bbbd26088e6454dc
Motko <77466238+Motko222@users.noreply.github.com>
2025-05-21 11:26:54 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,6 +10,7 @@ Wants=network-online.target
 
 [Service]
 EnvironmentFile=/root/scripts/nock/env
+Environment="PATH=/root/.cargo/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin:/root/go/bin:/usr/local/go/bin:/root/go/bin:/usr/local/go/bin:/root/go/bin"
 User=root
 Group=root
 ExecStart=cd /root/nockchain/target/release && make run-nockchain-leader
@@ -20,7 +21,7 @@ LimitNPROC=4096
 StandardOutput=journal
 StandardError=journal
 SyslogIdentifier=nock-leader
-WorkingDirectory=/root/nockchain/target/release
+WorkingDirectory=/root/nockchain
 
 [Install]
 WantedBy=multi-user.target" > /etc/systemd/system/nock-leader.service
@@ -33,9 +34,10 @@ Wants=network-online.target
 
 [Service]
 EnvironmentFile=/root/scripts/nock/env
+Environment="PATH=/root/.cargo/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin:/root/go/bin:/usr/local/go/bin:/root/go/bin:/usr/local/go/bin:/root/go/bin"
 User=root
 Group=root
-ExecStart=cd /root/nockchain/target/release && make run-nockchain-follower
+ExecStart=make run-nockchain-follower
 Restart=always
 RestartSec=30
 LimitNOFILE=65536
@@ -43,7 +45,7 @@ LimitNPROC=4096
 StandardOutput=journal
 StandardError=journal
 SyslogIdentifier=nock-follower
-WorkingDirectory=/root/nockchain/target/release
+WorkingDirectory=/root/nockchain
 
 [Install]
 WantedBy=multi-user.target" > /etc/systemd/system/nock-follower.service
