ad3944acd0553ed05b31782aceee4c78fc0dc079
Motko <77466238+Motko222@users.noreply.github.com>
2025-05-21 15:01:25 +0200

    Update stop.sh

--- stop.sh
+++ stop.sh
@@ -3,5 +3,4 @@
 path=$(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd) 
 folder=$(echo $path | awk -F/ '{print $NF}')
 
-systemctl stop nock-leader.service
-systemctl stop nock-follower.service
+systemctl stop $folder.service
