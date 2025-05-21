2a5da543924fd2704d3d69c50bdf01202d24910a
Motko <77466238+Motko222@users.noreply.github.com>
2025-05-21 15:01:48 +0200

    Update and rename start-follower.sh to start.sh

--- start.sh
+++ start.sh
@@ -3,6 +3,6 @@
 path=$(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd) 
 folder=$(echo $path | awk -F/ '{print $NF}')
 
-systemctl restart nock-follower.service
+systemctl restart $folder.service
 
-journalctl -n 200 -u nock-follower.service -f --no-hostname -o cat
+journalctl -n 200 -u $folder.service -f --no-hostname -o cat
