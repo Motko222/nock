4b4fcec6a8dd862b4e3cfd429a8e8c3ebf89bc3c
Motko <77466238+Motko222@users.noreply.github.com>
2025-05-21 10:50:28 +0200

    Update report.sh

--- report.sh
+++ report.sh
@@ -11,9 +11,10 @@ service1=$(sudo systemctl status nock-leader --no-pager | grep "active (running)
 service2=$(sudo systemctl status nock-follower --no-pager | grep "active (running)" | wc -l)
 errors1=$(journalctl -u nock-leader.service --since "1 hour ago" --no-hostname -o cat | grep -c -E "rror|ERR")
 errors2=$(journalctl -u nock-follower.service --since "1 hour ago" --no-hostname -o cat | grep -c -E "rror|ERR")
+balance=$(./root/nockchain/target/release/nockchain-wallet --nockchain-socket ./test-leader/nockchain.sock balance)
 
-status="ok" && message=""
-[ $errors1 -gt 500 ] && status="warning" && message="errors=$errors";
+status="ok" && message="bal=$balance"
+[ $errors1 -gt 500 ] && status="warning" && message="bal=$balance errors=$errors";
 [ $service2 -ne 1 ] && status="error" && message="follower service not running";
 [ $service1 -ne 1 ] && status="error" && message="leader service not running";
 
