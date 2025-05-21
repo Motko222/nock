efc68d3d91c99059f89891f8f97ab60dff048a79
Motko <77466238+Motko222@users.noreply.github.com>
2025-05-21 15:24:16 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,33 +2,9 @@ path=$(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd)
 folder=$(echo $path | awk -F/ '{print $NF}')
 source $path/env
 
-#leader
-printf "[Unit]
-Description=nock-leader node
-After=network.target
-Wants=network-online.target
-
-[Service]
-EnvironmentFile=/root/scripts/nock/env
-Environment="PATH=/root/.cargo/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin:/root/go/bin:/usr/local/go/bin:/root/go/bin:/usr/local/go/bin:/root/go/bin"
-User=root
-Group=root
-ExecStart=make run-nockchain-leader
-Restart=always
-RestartSec=30
-LimitNOFILE=65536
-LimitNPROC=4096
-StandardOutput=journal
-StandardError=journal
-SyslogIdentifier=nock-leader
-WorkingDirectory=/root/nockchain
-
-[Install]
-WantedBy=multi-user.target" > /etc/systemd/system/nock-leader.service
-
 #follower
 printf "[Unit]
-Description=nock-follower node
+Description=nock miner
 After=network.target
 Wants=network-online.target
 
@@ -44,11 +20,11 @@ LimitNOFILE=65536
 LimitNPROC=4096
 StandardOutput=journal
 StandardError=journal
-SyslogIdentifier=nock-follower
+SyslogIdentifier=nock
 WorkingDirectory=/root/nockchain
 
 [Install]
-WantedBy=multi-user.target" > /etc/systemd/system/nock-follower.service
+WantedBy=multi-user.target" > /etc/systemd/system/nock.service
 
 sudo systemctl daemon-reload
-sudo systemctl enable nock-leader
+sudo systemctl enable nock
