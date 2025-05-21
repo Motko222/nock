f9bc8b239b3814370936b5ee63c3fd8ae1737be1
Motko <77466238+Motko222@users.noreply.github.com>
2025-05-21 14:25:06 +0200

    Update and rename start.sh to start-leader.sh

--- start-leader.sh
+++ start-leader.sh
@@ -3,10 +3,6 @@
 path=$(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd) 
 folder=$(echo $path | awk -F/ '{print $NF}')
 
-systemctl stop nock-leader.service
-systemctl stop nock-follower.service
-
 systemctl restart nock-leader.service
-systemctl restart nock-follower.service
 
 journalctl -n 200 -u nock-leader.service -f --no-hostname -o cat
