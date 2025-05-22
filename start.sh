fce588c49e4fe2fb7036581d915cbe8a9ac24e08
Motko <77466238+Motko222@users.noreply.github.com>
2025-05-22 09:22:36 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 path=$(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd) 
 folder=$(echo $path | awk -F/ '{print $NF}')
 
-rm -rf /root/.socket/.data.nockchain /root/.socket/nockchain_npc.sock
+rm -rf /root/nockchain/.socket/.data.nockchain /root/nockchain/.socket/nockchain_npc.sock
 
 systemctl restart $folder.service
 
