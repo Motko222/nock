ef7346dc4a99c265e35fa882c92cf6ebbe9f368b
Motko <77466238+Motko222@users.noreply.github.com>
2025-05-22 17:54:19 +0200

    Create remove-service.sh

---
+++ remove-service.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+path=$(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd) 
+folder=$(echo $path | awk -F/ '{print $NF}')
+source $path/env
+
+sudo systemctl stop $folder
+sudo systemctl disable $folder
