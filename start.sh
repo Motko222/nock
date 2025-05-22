a43ef6ffd43876ce0328e84ccd23a7fd90183fd8
Motko <77466238+Motko222@users.noreply.github.com>
2025-05-22 08:27:06 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,6 +3,8 @@
 path=$(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd) 
 folder=$(echo $path | awk -F/ '{print $NF}')
 
+rm -rf /root/.socket/.data.nockchain /root/.socket/nockchain_npc.sock
+
 systemctl restart $folder.service
 
 journalctl -n 200 -u $folder.service -f --no-hostname -o cat
