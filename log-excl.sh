58aac39d8c3baf1432058111b6cdaa0840d3a0c5
Motko <77466238+Motko222@users.noreply.github.com>
2025-05-22 23:00:21 +0200

    Create log-excl.sh

---
+++ log-excl.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+path=$(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd) 
+folder=$(echo $path | awk -F/ '{print $NF}')
+
+journalctl -n 1000000 -u $folder.service -f --no-hostname -o cat | \
+ grep -v -E "friendship ended with|Handshake with the remote timed out|Failed outgoing connection|Failed incoming connection|command: timer"
