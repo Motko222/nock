8d6f39488a0fcfa358dc5805696fd0b1f67d814e
Motko <77466238+Motko222@users.noreply.github.com>
2025-05-22 17:00:15 +0200

    Update log-block.sh

--- log-block.sh
+++ log-block.sh
@@ -3,4 +3,4 @@
 path=$(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd) 
 folder=$(echo $path | awk -F/ '{print $NF}')
 
-journalctl -n 1000000 -u $folder.service -f --no-hostname -o cat | grep -E "added to validated blocks" -A 10
+journalctl -n 1000000 -u $folder.service -f --no-hostname -o cat | grep -E "generating new candidate block|Could not load mining kernel"
