c26430a6a0cce8182a599f50bc6c2d72c974fafc
Motko <77466238+Motko222@users.noreply.github.com>
2025-05-23 10:11:45 +0200

    Update log-excl.sh

--- log-excl.sh
+++ log-excl.sh
@@ -4,4 +4,4 @@ path=$(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd)
 folder=$(echo $path | awk -F/ '{print $NF}')
 
 journalctl -n 1000000 -u $folder.service -f --no-hostname -o cat | \
- grep -v -E "aborted by peer|friendship ended with|Handshake with the remote timed out|Failed outgoing connection|Failed incoming connection|command: timer|cryptographic handshake failed"
+ grep -v -E "Timeout has been reached|aborted by peer|friendship ended with|Handshake with the remote timed out|Failed outgoing connection|Failed incoming connection|command: timer|cryptographic handshake failed"
