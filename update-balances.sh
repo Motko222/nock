1b325daf4e9869326eb0ae538e5ca502fef98b0e
Motko <77466238+Motko222@users.noreply.github.com>
2025-05-23 08:54:28 +0200

    Update update-balances.sh

--- update-balances.sh
+++ update-balances.sh
@@ -4,5 +4,6 @@ path=$(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd)
 folder=$(echo $path | awk -F/ '{print $NF}')
 source $path/env
 
+RUST_LOG=ERROR
 cd /root/nockchain/target/release
 ./nockchain-wallet --nockchain-socket $WORKDIR/.socket/nockchain_npc.sock update-balance
