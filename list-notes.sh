9059daa2a223f28195bbed382225cf4cf97d5517
Motko <77466238+Motko222@users.noreply.github.com>
2025-05-23 08:54:44 +0200

    Update list-notes.sh

--- list-notes.sh
+++ list-notes.sh
@@ -4,5 +4,6 @@ path=$(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd)
 folder=$(echo $path | awk -F/ '{print $NF}')
 source $path/env
 
+RUST_LOG=ERROR
 cd /root/nockchain/target/release
 ./nockchain-wallet --nockchain-socket $WORKDIR/.socket/nockchain_npc.sock list-notes-by-pubkey -p $PUBKEY
