da70194f308bbe79e79c149235f71f8c065eddbf
Motko <77466238+Motko222@users.noreply.github.com>
2025-07-18 08:47:07 +0200

    Update report.sh

--- report.sh
+++ report.sh
@@ -21,7 +21,7 @@ cat >$json << EOF
   "updated":"$(date --utc +%FT%TZ)",
   "measurement":"report",
   "tags": {
-       "id":"$folder",
+       "id":"$folder-$ID",
        "machine":"$MACHINE",
        "grp":"node",
        "owner":"$OWNER"
